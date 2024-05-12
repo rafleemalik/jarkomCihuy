90a90af43bddd5b5c697f80decc7fcd67fd63123
youkwhd <lolywk@tutanota.com>
2024-05-12 20:27:05 +0700

    chore: add callback on serve

--- http/__init__.py
+++ http/__init__.py
@@ -1,18 +1,20 @@
-import socket, threading as thread, time, signal, sys
+import socket, threading as thread, signal, sys
 
-def init(port = 3000):
+def init(port=3000):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("127.0.0.1", port))
+    addr = ("127.0.0.1", port)
+    sock.bind(addr)
     sock.settimeout(1)
     sock.listen(1)
-    return {"socket": sock}
+    return {"socket": sock, "addr": addr}
 
-def serve(http):
+def serve(http, callback=lambda addr: None):
     def __sigint_handler(sig, frame):
         close(http)
         sys.exit(0)
 
     signal.signal(signal.SIGINT, __sigint_handler)
+    callback(http["addr"])
 
     while True:
         try:
--- jarkom.py
+++ jarkom.py
@@ -1,8 +1,8 @@
-import http, signal, sys
+import http
 
 def main():
     server = http.init()
-    http.serve(server)
+    http.serve(server, lambda addr: print(f"Listening on http://{addr[0]}:{addr[1]}"))
 
 if __name__ == "__main__":
     main()
