585cdf23925a4900853a3f5f999648507873cb4a
youkwhd <lolywk@tutanota.com>
2024-05-13 18:20:30 +0700

    chore: reconfig serve callback args

--- http/__init__.py
+++ http/__init__.py
@@ -1,4 +1,4 @@
-import socket, threading as thread, signal, sys
+import socket, signal, sys
 
 def init(port=3000):
     addr = ("127.0.0.1", port)
@@ -8,13 +8,13 @@ def init(port=3000):
     sock.listen(1)
     return {"socket": sock, "addr": addr}
 
-def serve(http, callback=lambda addr: None):
+def serve(http, callback=lambda host, port: None):
     def __sigint_handler(sig, frame):
         close(http)
         sys.exit(0)
 
     signal.signal(signal.SIGINT, __sigint_handler)
-    callback(http["addr"])
+    callback(http["addr"][0], http["addr"][1])
 
     while True:
         (csock, addr) = http["socket"].accept()
